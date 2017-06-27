801e600d825f712cc1d398fe1464a90410bc6fc4
Ankit Srivastava <sankit@student.ethz.ch>
2017-06-27 21:12:54 +0200

    LR=1e-7

--- rsrcnn/vgg16.py
+++ rsrcnn/vgg16.py
@@ -13,7 +13,7 @@ from tqdm import tqdm
 import time
 import re
 
-tf.app.flags.DEFINE_float("learning_rate"               , 1e-6 , "Learning rate.")
+tf.app.flags.DEFINE_float("learning_rate"               , 1e-7 , "Learning rate.")
 tf.app.flags.DEFINE_float("momentum"                    , 0.9  , "Momentum")
 tf.app.flags.DEFINE_float("max_gradient_norm"           , 5.0   , "Clip gradients to this norm.")
 
